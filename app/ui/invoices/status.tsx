41d3a717a329daea4459040067fd6f3cc507e9e2
Cecily Toro <torocecily@gmail.com>
2024-01-11 13:56:27 -0500

    initial commit

---
+++ app/ui/invoices/status.tsx
@@ -0,0 +1,31 @@
+import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+// clsx is a library that lets you conditionally apply classes easily:
+import clsx from 'clsx';
+
+export default function InvoiceStatus({ status }: { status: string }) {
+  return (
+    <span
+      // example of how you would use clsx library to conditionally render class names:
+      className={clsx(
+        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        {
+          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-green-500 text-white': status === 'paid',
+        },
+      )}
+    >
+      {status === 'pending' ? (
+        <>
+          Pending
+          <ClockIcon className="ml-1 w-4 text-gray-500" />
+        </>
+      ) : null}
+      {status === 'paid' ? (
+        <>
+          Paid
+          <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+    </span>
+  );
+}
